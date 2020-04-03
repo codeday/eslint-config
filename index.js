30e900397b6b2d54dd632ee0c513d06c0f5f8176
Tyler Menezes <tylermenezes@gmail.com>
2020-04-03 11:21:17 -0400

    Add sonar, no-secrets, and node

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 module.exports = {
-  extends: [ 'airbnb', 'plugin:jsdoc/recommended'],
-  plugins: [
-      'jsdoc',
-  ],
+  plugins: [ 'jsdoc', 'sonarjs', 'no-secrets'],
+  extends: [ 'airbnb', 'plugin:jsdoc/recommended', 'plugin:sonarjs/recommended'],
   settings: {
     jsdoc: {
       ignorePrivate: true,
     },
     tagNamePreference: {
-      "augments": "extends",
+      'augments': 'extends',
     }
   },
   rules: {
+    'no-secrets/no-secrets': ['error'],
     'quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
     'max-len': ['error', 120],
     'react/static-property-placement': ['error', 'static public field'],
-    'react/jsx-filename-extension': ['error', { "extensions": ['.js', '.ts'] }],
+    'react/jsx-filename-extension': ['error', { 'extensions': ['.js', '.ts'] }],
     'react/forbid-prop-types': ['off'],
     'react/prefer-stateless-function': ['off'],
     'react/jsx-props-no-spreading': ['off'],
     'react/jsx-one-expression-per-line': ['off'],
     'react/state-in-constructor': ['off'],
-    'jsdoc/check-examples': ['error', { paddedIndent: 2 }],
     'class-methods-use-this': ['off'],
     'comma-dangle': ['error', {
         'arrays': 'always-multiline',
@@ -32,9 +30,23 @@ module.exports = {
       }
     ],
 
+    'node/no-deprecated-api': ['error'],
+    'node/no-extraneous-import': ['error'],
+    'node/no-extraneous-require': ['error'],
+    'node/no-exports-assign': ['error'],
+    'node/no-missing-import': ['error'],
+    'node/no-missing-require': ['error'],
+    'node/no-unpublished-bin': ['error'],
+    'node/no-unpublished-import': ['error'],
+    'node/no-unpublished-require': ['error'],
+    'node/process-exit-as-throw': ['error'],
+    'node/shebang': ['error'],
+
+    'jsdoc/check-examples': ['error', { paddedIndent: 2 }],
+
     // Fix for broken autofix in jsdoc
-    "jsdoc/require-jsdoc": [0],
-    "require-jsdoc": [1, {require: {
+    'jsdoc/require-jsdoc': [0],
+    'require-jsdoc': [1, {require: {
       ArrowFunctionExpression: true,
       FunctionExpression: true,
       MethodDefinition: true,
