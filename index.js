d506748cbacb74e860109e6cb515c6845ade4e03
Tyler Menezes <tylermenezes@gmail.com>
2020-04-03 10:22:10 -0400

    Update to add jsdoc requirements

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 module.exports = {
-  extends: 'airbnb',
+  extends: [ 'airbnb', 'plugin:jsdoc/recommended'],
+  plugins: [
+      'jsdoc',
+  ],
+  settings: {
+    jsdoc: {
+      ignorePrivate: true,
+    },
+    tagNamePreference: {
+      "augments": "extends",
+    }
+  },
   rules: {
     'quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
     'max-len': ['error', 120],
@@ -10,6 +21,7 @@ module.exports = {
     'react/jsx-props-no-spreading': ['off'],
     'react/jsx-one-expression-per-line': ['off'],
     'react/state-in-constructor': ['off'],
+    'eslint/check-examples': ['error', { paddedIndent: 2 }],
     'class-methods-use-this': ['off'],
     'comma-dangle': ['error', {
         'arrays': 'always-multiline',
