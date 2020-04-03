84f04a1cd93f92ca151d20e778ce20a9e914dcb4
Tyler Menezes <tylermenezes@gmail.com>
2020-04-03 10:23:47 -0400

    Fix typo

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ module.exports = {
     'react/jsx-props-no-spreading': ['off'],
     'react/jsx-one-expression-per-line': ['off'],
     'react/state-in-constructor': ['off'],
-    'eslint/check-examples': ['error', { paddedIndent: 2 }],
+    'jsdoc/check-examples': ['error', { paddedIndent: 2 }],
     'class-methods-use-this': ['off'],
     'comma-dangle': ['error', {
         'arrays': 'always-multiline',
